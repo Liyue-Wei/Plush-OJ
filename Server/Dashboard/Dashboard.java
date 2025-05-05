cc28b51c147242ae6dff2afff09424b1f694fe72
Liyue <eric20050107@gmail.com>
2025-05-05 13:33:51 +0800

    測試

---
+++ Server/Dashboard/Dashboard.java
@@ -0,0 +1,12 @@
+package Dashboard;
+import javax.swing.*;
+import java.util.*;
+
+public class Dashboard {
+    public static void main(String[] args)  {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        String name = sc.next();
+        System.out.println("Hello " + name);
+        System.out.println("Hello World");
+    }
+}
