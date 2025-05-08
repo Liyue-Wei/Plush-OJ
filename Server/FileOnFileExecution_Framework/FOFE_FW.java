ee2dde8a6d32951c29f8dbaebd1a0161936e7e91
Zhang Zhewei <126851525+Liyue-Wei@users.noreply.github.com>
2025-05-08 23:49:21 +0800

    Update FOFE_FW.java

--- Server/FileOnFileExecution_Framework/FOFE_FW.java
+++ Server/FileOnFileExecution_Framework/FOFE_FW.java
@@ -1,4 +1,7 @@
-package FileOnFileExecution_Framework.FF_Container;
+package FileOnFileExecution_Framework;
 
 public class FOFE_FW {
+    public static void main(String[] args) {
+        System.out.println("File On File Execution Framework");
+    }
 }
