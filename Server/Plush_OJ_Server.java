b670db391cd498239e5dba9ae352f017331cad90
Zhang Zhewei <126851525+Liyue-Wei@users.noreply.github.com>
2025-05-12 16:18:32 +0800

    Update Plush_OJ_Server.java

--- Server/Plush_OJ_Server.java
+++ Server/Plush_OJ_Server.java
@@ -1,10 +1,11 @@
 import java.io.File;
 
-public class Plush_OJ {
+public class Plush_OJ_Server {
     public static void main(String[] args) {
         System.out.println("Welcome to Plush::OJ Server!");
 
         File Path = new File("");
         String rootPath = new String(Path.getAbsolutePath());
+        System.out.println("Root Path: " + rootPath);
     }
 }
