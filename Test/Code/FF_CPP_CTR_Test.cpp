835681c67e9daa35ca9a06e95d9fb7a58e27d2db
Zhang Zhewei <126851525+Liyue-Wei@users.noreply.github.com>
2025-05-14 16:13:28 +0800

    Create FF_CPP_CTR_Test.cpp

---
+++ Test/Code/FF_CPP_CTR_Test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <cstdlib>
+
+struct funcIO {
+    std::string fpIN = "./Temp/TempCode.cpp";    // InFile Path
+    int tdQTY;    // Test DATA Quantity
+    std::string tdARR[];    // Test DATA Array
+};
+
+int main() {
+    std::cout << "Hello World\n";
+
+    return 0;
+}
