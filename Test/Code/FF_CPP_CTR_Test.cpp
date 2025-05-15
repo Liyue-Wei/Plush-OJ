e1581ae530fbec3313fcb0f70d3b6caae58f6abc
Zhang Zhewei <126851525+Liyue-Wei@users.noreply.github.com>
2025-05-15 11:04:21 +0800

    Update FF_CPP_CTR_Test.cpp

--- Test/Code/FF_CPP_CTR_Test.cpp
+++ Test/Code/FF_CPP_CTR_Test.cpp
@@ -6,7 +6,6 @@
 struct TempCase {
     std::string tcPath;  // Temp Code Path
     std::string tdPath;  // Test DATA Path
-
     void parseArgs(char* argv[]) {
         tcPath = argv[1];
         tdPath = argv[2];
@@ -21,15 +20,19 @@ int main(int argc, char* argv[]) {
 
     TempCase TC;
     TC.parseArgs(argv);
-
     std::ifstream inFile_TC(TC.tcPath, std::ios::in);
     if (!inFile_TC) {
         std::cout << "An Error occured when reading Temp Code, Terminated...\n";
         return 2;  // Error Code 2
+    } else {
+        std::string str;
+        while (std::getline(inFile_TC, str)) {
+            std::cout << str << std::endl;
+        }
     }
 
-    std::string command = TC.tcPath + " < " + TC.tdPath;  // 這邊要先把測資獨立成一個檔案，一次一次測
-    std::system(command.c_str());
+    // std::string command = TC.tcPath + " < " + TC.tdPath;  // 這邊要先把測資獨立成一個檔案，一次一次測
+    // std::system(command.c_str());
     
     return 0;
 }
