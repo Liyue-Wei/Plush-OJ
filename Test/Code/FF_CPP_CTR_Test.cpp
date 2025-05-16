9e086ea8cf3d77b3b7dbeee3a94052b59e283450
Zhang Zhewei <126851525+Liyue-Wei@users.noreply.github.com>
2025-05-16 16:12:29 +0800

    Update FF_CPP_CTR_Test.cpp

--- Test/Code/FF_CPP_CTR_Test.cpp
+++ Test/Code/FF_CPP_CTR_Test.cpp
@@ -83,5 +83,5 @@ Error Code 3 : Prohibited Header Detected
 Error Code 4 : Compile Error
 
 g++ -o C:\Users\eric2\Desktop\Plush-OJ\Test\Code\FF_CPP_CTR_Test C:\Users\eric2\Desktop\Plush-OJ\Test\Code\FF_CPP_CTR_Test.cpp
-C:\Users\eric2\Desktop\Plush-OJ\Test\Code\FF_CPP_CTR_Test.exe C:\Users\eric2\Desktop\Plush-OJ\Test\Temp_Code\Hello.cpp 10 C:\Users\eric2\Desktop\Plush-OJ\Test\Temp_JSON\PL.json
+C:\Users\eric2\Desktop\Plush-OJ\Test\Code\FF_CPP_CTR_Test.exe 
 */
