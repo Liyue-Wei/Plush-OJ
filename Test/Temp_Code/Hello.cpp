447ebf366f34fc61448b548725777750d17216b4
Zhang Zhewei <126851525+Liyue-Wei@users.noreply.github.com>
2025-05-18 00:32:30 +0800

    Update Hello.cpp

--- Test/Temp_Code/Hello.cpp
+++ Test/Temp_Code/Hello.cpp
@@ -3,7 +3,7 @@ int main() {
     printf("Hello World\n");
     int i, n, t;
     scanf("%d %d %d", &i, &n, &t);
-    printf("%d, %d, %d", i, n, t);
+    printf("%d, %d, %d\n", i, n, t);
 
     return 0;
 }
