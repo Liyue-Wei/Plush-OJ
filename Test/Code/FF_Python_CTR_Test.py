f927936ead15fae06e90fda0ce5006d445174c6c
Zhang Zhewei <126851525+Liyue-Wei@users.noreply.github.com>
2025-05-21 20:50:11 +0800

    Update FF_Python_CTR_Test.py

--- Test/Code/FF_Python_CTR_Test.py
+++ Test/Code/FF_Python_CTR_Test.py
@@ -6,7 +6,7 @@ import time
 class CPP_CTR:
     args = ["currPath", "tcPath", "TT", "tdPath", "taPath", "QN", "UID", "Time"]
 
-def main(argv):
+def main():
     currPath = os.path.abspath(os.path.dirname(__file__))
 
 if __name__ == '__main__':
