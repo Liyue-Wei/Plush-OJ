da090f61c273bf325c924a3a57663a9592c61024
Zhang Zhewei <126851525+Liyue-Wei@users.noreply.github.com>
2025-05-22 14:09:54 +0800

    Update FF_Python_CTR_Test.py

--- Test/Code/FF_Python_CTR_Test.py
+++ Test/Code/FF_Python_CTR_Test.py
@@ -5,6 +5,8 @@ import os
 import sys
 import subprocess
 
+global currPath, tempCode, testData, info
+
 def parseTD(TDPath):
     try:
         with open(TDPath, 'r', encoding='utf-8') as TD:
@@ -31,6 +33,31 @@ class CPP_CTR:
 def main():
     currPath = os.path.abspath(os.path.dirname(__file__))
     args = [str(sys.argv[1]), str(sys.argv[2]), str(sys.argv[3])]  # TempCode Path, TestData Path, Info
+    if len(args) != 3:
+        print("Invalid args Input, Terminated...")
+        return 1  # Error Code 1
+    
+    try:
+        with open(args[0], 'r', encoding='utf-8') as TC:
+            tempCode = TC.read()
+    except FileNotFoundError:
+        print("An Error occured when reading Temp Code, Terminated...")
+        return 2  # Error Code 2
+    
+    
 
 if __name__ == '__main__':
     main()
+
+'''
+Error Code 1 : Invalid args Input
+Error Code 2 : Reading Temp Code
+Error Code 3 : Prohibited Header Detected
+Error Code 4 : Compile Error
+Error Code 5 : System Error - Shell can't be opened
+Error Code 6 : Unknow Test Type
+Error Code 7 : 
+Error Code 8 : 
+Error Code 9 : 
+Error Code 10: 
+'''
