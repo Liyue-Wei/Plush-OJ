da6cfdbe67a13c061a355002b58531a52b6c51a5
Zhang Zhewei <126851525+Liyue-Wei@users.noreply.github.com>
2025-05-26 16:46:13 +0800

    UEOF Judge 完成

--- Test/Temp_Code/QN008A-XXXXX-0000-00-00.cpp
+++ Test/Temp_Code/QN008A-XXXXX-0000-00-00.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 using namespace std;
+
+// 判斷是否為質數的函式
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i * i <= n; ++i) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
 int main() {
-    string str;
-    cin >> str;
-    cout << "Hello " << str << "!\n";
+    int num;
+    cin >> num;
+
+    if (isPrime(num)) {
+        cout << "Yes\n";
+    } else {
+        cout << "No\n";
+    }
 
     return 0;
 }
