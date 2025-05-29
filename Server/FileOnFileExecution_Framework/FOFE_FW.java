7f7e1b1f594e9723eff38910ab9f3d2e8e1d352c
Zhang Zhewei <126851525+Liyue-Wei@users.noreply.github.com>
2025-05-30 00:56:05 +0800

    Update FOFE_FW.java

---
+++ Server/FileOnFileExecution_Framework/FOFE_FW.java
@@ -0,0 +1,12 @@
+package FileOnFileExecution_Framework;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+
+public class FOFE_FW {
+    
+}
