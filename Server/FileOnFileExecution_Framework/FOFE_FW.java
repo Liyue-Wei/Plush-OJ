0bb5432e3f4f644ae2e2c2076572080360ece9df
Zhang Zhewei <126851525+Liyue-Wei@users.noreply.github.com>
2025-05-30 01:11:28 +0800

    Update FOFE_FW.java

--- Server/FileOnFileExecution_Framework/FOFE_FW.java
+++ Server/FileOnFileExecution_Framework/FOFE_FW.java
@@ -8,5 +8,31 @@ import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 public class FOFE_FW {
-    
+    private static class StreamGobbler implements Runnable {
+        private final InputStreamReader inputStreamReader;
+        private final Consumer<String> consumer;
+
+        public StreamGobbler(InputStreamReader inputStreamReader, Consumer<String> consumer) {
+            this.inputStreamReader = inputStreamReader;
+            this.consumer = consumer;
+        }
+
+        @Override
+        public void run() {
+            new BufferedReader(inputStreamReader).lines().forEach(consumer);
+        }
+    }
+
+    public static void main(String[] args) {
+        Process process = null;
+        try (java.util.concurrent.ExecutorService executor = Executors.newFixedThreadPool(2)) {
+            ProcessBuilder commandBuilder = new ProcessBuilder(
+                    "python",
+                    "Server/FileOnFileExecution_Framework/FF_Container/FF_Python_CTR.py",
+                    "Test/Temp_Code/QN001A-XXXXX-0000-00-00.cpp",
+                    "Test/Temp_JSON/QN001A.json",
+                    "QN001A-XXXXX-0000-00-00"
+            );
+        }
+    }
 }
