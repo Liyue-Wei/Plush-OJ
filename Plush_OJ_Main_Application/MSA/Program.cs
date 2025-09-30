2b71787f4634d10a1df660710a5612cb89360a6e
Zhang Zhewei <126851525+Liyue-Wei@users.noreply.github.com>
2025-10-01 01:01:19 +0800

    Update Program.cs

--- Plush_OJ_Main_Application/MSA/Program.cs
+++ Plush_OJ_Main_Application/MSA/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿/* Copyright (c) 2024 Zhang Zhewei (Liyue-Wei) - Licensed under the MIT License. */
+
+using System;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Net;
